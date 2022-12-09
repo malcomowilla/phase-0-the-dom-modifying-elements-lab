e9ff6b9699c1fd6eb271b8e25e54faf8aa667394
malcomowilla <owillamalcom@gmail.com>
2022-12-09 18:02:54 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your code here!
+
+function removeNode() {
+  let main = document.querySelector('main').remove()
+  console.log(main)
+
+  
+}
+removeNode(Node)
+
+ const newHeader = document.createElement('h1');
+ h1.id='victory'
+ 
+ 
+
+
+
+h1.innerHTML= 
+
+
+//const element = document.createElement("div");
+//div.remove();
+
+//document.body.append("element")
+
+//const ul = document.createElement("ul");
+
+//for (let i = 0; i < 3; i++) {
+  //const li = document.createElement("li");
+  //li.textContent = (i + 1).toString();
+  //ul.append(li);
+//}
+
+//element.append(ul);
+
+
+//const input = document.getElementById('input');
+//input.addEventListener('click', function() {
+  //alert('I was clicked!');
+//});
+
+
+
+//addEventListener('DOMContentLoaded', (event) => {});
+
+
+//Delaying DOMContentLoaded
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
