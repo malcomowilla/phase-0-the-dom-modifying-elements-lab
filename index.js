9ba479030aa8fe023dec11995451e2ba006e4a8e
malcomowilla <owillamalcom@gmail.com>
2023-09-19 15:28:29 +0300

    revisting javascriptdom

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // Write your code here!
 
-function removeNode() {
-  let main = document.querySelector('main').remove()
-  console.log(main)
+// function removeNode() {
+//   let main = document.querySelector('main').remove()
+//   console.log(main)
 
   
-}
-removeNode(Node)
+// }
+// removeNode(Node)
 
- const newHeader = document.createElement('h1');
- h1.id='victory'
+//  const newHeader = document.createElement('h1');
+//  h1.id='victory'
  
  
 
 
 
-h1.innerHTML= 
+// h1.innerHTML= 
 
 
 //const element = document.createElement("div");
@@ -46,8 +46,295 @@ h1.innerHTML=
 
 //Delaying DOMContentLoaded
 
+// const element = document.createElement('div')
 
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-});
+// document.body.append(element)
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement('li')
+//   li.textContent = (i + 1).toString()
+//   ul.append(li)
+// }
+
+
+// element.append(ul)
+
+
+
+
+
+
+
+
+
+// const element2 = document.getElementById('main')
+
+// element2.style.height = '300px'
+// element2.style.backgroundColor = "red";
+
+
+// element.style.lineHeight = 3;
+// element2.textContent = 'Youve changed whats on the screen!'
+
+// element2.style.marginLeft = '10em'
+
+
+// element2.className = 'pet-listing dog'
+
+// element2.classList
+
+
+
+
+// ul = document.querySelector('ul')
+
+//   const secondChild = ul.querySelector('li:nth-child(2)')
+//   ul.removeChild(secondChild)
+
+
+  const newHeader = document.createElement('h1')
+
+  newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+newHeader.textContent = "is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
